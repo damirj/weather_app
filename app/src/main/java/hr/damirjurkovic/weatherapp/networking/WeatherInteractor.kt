26caec0846915f86457333983779360720b6d2dd
damirj <d.jurkovic934@gmail.com>
2020-06-23 00:36:13 +0200

    prepare for pr

--- app/src/main/java/hr/damirjurkovic/weatherapp/networking/WeatherInteractor.kt
+++
@@ -1,7 +0,0 @@
-package hr.damirjurkovic.weatherapp.networking
-
-import hr.damirjurkovic.weatherapp.networking.response.WeatherResponse
-
-interface WeatherInteractor {
-    suspend fun getWeather(location: String): WeatherResponse
-}
